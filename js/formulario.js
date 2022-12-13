006d4c94e0a28746a651b37a870c2b75881c2d40
David <dreypad2605@iesmartinezm.es>
2022-12-13 11:21:04 +0100

    Get Post

---
+++ js/formulario.js
@@ -0,0 +1,11 @@
+const url = "https://formsubmit.co/ajax/jperric300@iesmartinezm.es";
+const form = document.querySelector(".miFormulario");
+const post = (url, body) => fetch(url, { method: "POST", body });
+form.addEventListener("submit", function (ev) {
+  ev.preventDefault(); //para no actualizar la página
+  const data = new FormData(form);
+  post(url, data)
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+    .catch((error) => console.error("ERROR" + error));
+});
--- js/pruebaRandom.js
+++ js/pruebaRandom.js
@@ -1,4 +1,4 @@
-fetch('https://randomuser.me/api/')
-.then(response => response.json())
-.then(data => console.log(data.results[0]))
-.catch(error=>console.error("Error"+error))
+fetch("https://randomuser.me/api/")
+  .then((response) => response.json())
+  .then((data) => console.log(data.results[0]))
+  .catch((error) => console.error("Error" + error));
