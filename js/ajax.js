2cd202cb82ef52c60041b1ea067a5de4c6e71ea5
David <dreypad2605@iesmartinezm.es>
2022-12-20 12:07:06 +0100

    AJAX

---
+++ js/ajax.js
@@ -0,0 +1,187 @@
+// constantes
+const $btn = document.querySelector("button"); // boton primer formulario mostrar usuario
+const $btn2 = document.getElementById("boton_objeto"); // botón segundo formulario enviar usuario
+const $btn3 = document.getElementById("boton_cambiar"); // botón tercero cambiar usuario
+const $btn4 = document.getElementById("boton_eliminar"); // último botón de eliminar
+const urlReqres = "https://reqres.in/api/users/";
+
+// acceso API
+
+let user = 1;
+let users = 0;
+let responseOK = false;
+let responseOK_ = false;
+
+/** Mostrar todos */
+fetch(urlReqres)
+  .then((response) => {
+    responseOK = response.ok;
+    return response.json();
+  })
+  .then((data) => {
+    users = data["total"];
+    if (responseOK) {
+      while (user <= users) {
+        let urlUser = urlReqres + user;
+        llamar(urlUser);
+        user++;
+      }
+    }
+    return data;
+  })
+  .catch((error) => console.log("Error" + error));
+
+/** Mostrar el seleccionado */
+const seleccionarUsuario = (e) => {
+  responseOK = false;
+  e.preventDefault();
+  $input = document.getElementById("num_usuario");
+  let id = $input.value;
+  let urlUser = urlReqres + id;
+  fetch(urlUser)
+    .then((response) => {
+      responseOK = response.ok;
+      return response.json();
+    })
+    .then((data) => {
+      if (responseOK) {
+        let email = data["data"]["email"];
+        let first_name = data["data"]["first_name"];
+        let last_name = data["data"]["last_name"];
+        let avatar = data["data"]["avatar"];
+        mostrarUsuario(email, first_name, last_name, avatar);
+      } else {
+        console.log("No pudo completarse la carga del usuario");
+      }
+    })
+    .catch((error) => console.log("Error : " + error));
+};
+
+const llamar = (url) => {
+  fetch(url)
+    .then((response) => {
+      responseOK_ = response.ok;
+      return response;
+    })
+    .then((response) => response.json())
+    .then((data_) => {
+      if (responseOK_) {
+        crearTarjeta(data_["data"]["first_name"], data_["data"]["avatar"]);
+      }
+    })
+    .catch((error) => console.log("Error" + error));
+};
+
+// vista
+
+const crearTarjeta = (nombre, imagen) => {
+  let $tarjeta = document.createElement("article");
+  document.body.insertAdjacentElement("afterbegin", $tarjeta);
+  let $img = document.createElement("img");
+  $img.src = imagen;
+  $img.alt = "Avatar de " + nombre;
+  $tarjeta.insertAdjacentElement("afterbegin", $img);
+  let $p = document.createElement("p");
+  $p.textContent = nombre;
+  $tarjeta.insertAdjacentElement("beforeend", $p);
+};
+
+const mostrarUsuario = (email, nombre, apellido, avatar) => {
+  let $divShow = document.getElementById("show");
+  let $tarjeta = document.createElement("article");
+  $divShow.insertAdjacentElement("afterbegin", $tarjeta);
+  let $img = document.createElement("img");
+  $img.src = avatar;
+  $img.alt = "Usuario seleccionado: " + nombre;
+  $tarjeta.insertAdjacentElement("afterbegin", $img);
+  let $pApellido = document.createElement("p");
+  $pApellido.textContent = apellido;
+  $tarjeta.insertAdjacentElement("beforeend", $pApellido);
+  let $pNombre = document.createElement("p");
+  $pNombre.textContent = nombre;
+  $tarjeta.insertAdjacentElement("beforeend", $pNombre);
+  let $pEmail = document.createElement("p");
+  $pEmail.textContent = email;
+  $tarjeta.insertAdjacentElement("beforeend", $pEmail);
+};
+
+// nuevo usuario
+
+const addUsuario = (e) => {
+  // evitar que se envíe el formulario
+  e.preventDefault();
+
+  // recoger valores del formulario
+  let email = document.getElementById("email").value;
+  let first_name = document.getElementById("first_name").value;
+  let last_name = document.getElementById("last_name").value;
+  let avatar = document.getElementById("avatar").value;
+
+  // crear el objeto Usuario
+  let newUser = {
+    email: email,
+    first_name: first_name,
+    last_name: last_name,
+    avatar: avatar,
+  };
+
+  // guardar el objeto Usuario en Local Storage
+  localStorage.setItem("usuario", newUser);
+
+  // Enviar usuario
+  fetch("https://reqres.in/api/users", {
+    method: "POST",
+    body: JSON.stringify({ newUser }),
+  })
+    .then((response) => {
+      console.log(response.status);
+      return response;
+    })
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+};
+
+const actualizarUsuario = (e) => {
+  // evitar que se envíe el formulario
+  e.preventDefault();
+
+  // recoger valores del formulario
+  let email = document.getElementById("email_change").value;
+
+  // importar usuario del local Storage
+  let usuario = localStorage.getItem("usuario");
+  // cambiar en el objeto usuario, el email
+  usuario.email = email;
+
+  // Actualizar email del usuario
+  fetch("https://reqres.in/api/users/13", {
+    method: "PUT",
+    body: JSON.stringify({ user: usuario }),
+  })
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+};
+
+const eliminarUsuario = (e) => {
+  // evitar que se envíe el formulario
+  e.preventDefault();
+
+  // recoger valores del formulario
+  let id = document.getElementById("id_usuario").value;
+
+  // eliminar del local storage
+  localStorage.removeItem("usuario");
+
+  // Eliminar
+  fetch(`https://reqres.in/api/users/${id}`, {
+    method: "DELETE",
+  })
+    .then((response) => console.log(response.status))
+    .catch((error) => console.log("Error : " + error));
+};
+
+// listeners
+$btn.addEventListener("click", seleccionarUsuario);
+$btn2.addEventListener("click", addUsuario);
+$btn3.addEventListener("click", actualizarUsuario);
+$btn4.addEventListener("click", eliminarUsuario);
--- js/b.js
+++ js/b.js
@@ -1,35 +1,44 @@
-const contenedor = document.querySelector(".container");
+let input = document.querySelector("input");
+
+input.addEventListener("submit", function (e) {
+  let id = input.value; //e.target.value
+
+  if (id > 0 && id < 13) {
+    fetch(`https://reqres.in/api/users/${id}`)
+      .then((response) => response.json())
+      .then((miembro) => ejercicio(miembro))
+      .catch((error) => console.error("ERROR en el sistema!!!!!!!!!!" + error));
+  } else {
+    ponerMensaje("El id debe estar entre 1 y 12", boton);
+  }
+});
 
 function ejercicio(jsonObj) {
-  const personas = jsonObj["data"];
-  for (var i = 0; i < personas.length; i++) {
-    const imagen = document.createElement("img");
-    const nombre = document.createElement("p");
-    const apellido = document.createElement("p");
-    const email = document.createElement("p");
+  let usuario = jsonObj["data"];
+  let contenedor = document.querySelector(".container");
 
-    imagen.src = personas[i].avatar;
-    nombre.textContent = personas[i].first_name;
-    apellido.textContent = personas[i].last_name;
-    email.textContent = personas[i].email;
+  let imagen = document.createElement("img");
+  let nombre = document.createElement("p");
+  let apellido = document.createElement("p");
+  let email = document.createElement("p");
 
-    contenedor.appendChild(imagen);
-    contenedor.appendChild(nombre);
-    contenedor.appendChild(apellido);
-    contenedor.appendChild(email);
-  }
+  imagen.setAttribute("src", usuario.avatar);
+  nombre.textContent("src", usuario.first_name);
+  apellido.textContent("src",usuario.last_name);
+  email.textContent("src", usuario.email);
+
+  contenedor.appendChild(imagen);
+  contenedor.appendChild(nombre);
+  contenedor.appendChild(apellido);
+  contenedor.appendChild(email);
 }
 
-fetch("https://reqres.in/api/users?page=1")
-  .then((response) => response.json())
-  .then((users) => {
-    ejercicio(users);
-  })
-  .catch((error) => console.error("ERROR en el sistema!!!!!!!!!!" + error));
-
-fetch("https://reqres.in/api/users?page=2")
-  .then((response) => response.json())
-  .then((users) => {
-    ejercicio(users);
-  })
-  .catch((error) => console.error("ERROR en el sistema!!!!!!!!!!" + error));
+const ponerMensaje = (mensaje, posicion) => {
+  let p = document.createElement("p");
+  p.textContent = mensaje;
+  p.style.color = "red";
+  posicion.insertAdjacentElement("afterend", p);
+  setTimeout(() => {
+    p.remove();
+  }, 3000);
+};
